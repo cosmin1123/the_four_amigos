0bd66773cd50ff6767ca3036557f96bab59635b7
cosmin <cosmin.didii@gmail.com>
2013-10-19 16:06:08 +0300

    initial commit

---
+++ gen/com/example/the_four_amigos/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.the_four_amigos;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
