e91754887cf370b30db3c9ff245745baf1ec16e5
Vitalie MALDUR <maldur.vitalik@gmail.com>
2013-10-20 04:28:24 +0300

    Mesaj

--- src/com/the_four_amigos/panic_helper/MainActivity.java
+++ src/com/the_four_amigos/panic_helper/MainActivity.java
@@ -7,10 +7,13 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.speech.tts.TextToSpeech;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 import com.the_four_amigos.panic_helper.alerts.Speak;
-import com.the_four_amigos.panic_helper.sensors.Acceleration;
+import com.the_four_amigos.panic_helper.sensors.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
     private SensorManager mSensorManager;
     public static boolean running;
     private static Context context;
@@ -20,8 +23,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Intent accelerationService = new Intent(this, Acceleration.class);
-        startService(accelerationService);
+        Switch s = (Switch) findViewById(R.id.Switch);
+        if(s != null) s.setOnCheckedChangeListener(this);
+
+        //Intent accelerationService = new Intent(this, Acceleration.class);
+        //startService(accelerationService);
+
+        Intent voice = new Intent(this, SpeechService.class);
+        startService(voice);
 
 
         MainActivity.context = getApplicationContext();
@@ -33,6 +42,15 @@ public class MainActivity extends Activity {
     public void setConfiguration(View view) {
         Intent intent = new Intent(this, ConfigurationActivity.class);
         startActivity(intent);
+    }
+
+    public void onHelp(View view) {
+        Toast.makeText(this, "Help",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Toast.makeText(this, "Monitored switch is " + (isChecked ? "on" : "off"),
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
