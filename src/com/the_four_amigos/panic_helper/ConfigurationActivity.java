66e11da3bb85c4fab78c77e5ab0da49c38c36406
Bugaa92 <bugaaa92@gmail.com>
2013-10-20 11:13:13 +0300

    Salvare intre sesiuni

--- src/com/the_four_amigos/panic_helper/ConfigurationActivity.java
+++ src/com/the_four_amigos/panic_helper/ConfigurationActivity.java
@@ -9,18 +9,27 @@ import android.view.DragEvent;
 import android.view.View;
 import android.widget.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class ConfigurationActivity extends Activity {
 
-    private   ArrayAdapter<String> spinnerAdapter;
-    private  AlertDialog.Builder alert;
-    private Spinner spinner;
     private final static float ACCELERATION = 9.8f;
     private final static float MPS_TO_MKPH = 3.6f;
-    public SeekBar gravitySeekBar;
-    public TextView accelerationTextView;
-    public TextView speedTextView;
+    private int index = 0;
+
+    private  ArrayAdapter<String> spinnerAdapter;
+    private  AlertDialog.Builder alert;
+
+    private Spinner spinner;
+    private SeekBar gravitySeekBar;
+    private Button saveButton, cancelButton;
+
+    private TextView accelerationTextView;
+    private TextView speedTextView;
+    private EditText mySmsText;
+
+    private Switch[] switches;
 
     public float getSpeedFromGravity (float gravity) {
         return (float)(Math.sqrt(2 * gravity * ACCELERATION)) * MPS_TO_MKPH;
@@ -29,15 +38,6 @@ public class ConfigurationActivity extends Activity {
     public void setGravityAndSpeed(float progress) {
         accelerationTextView.setText(Float.toString(progress + 1));
         speedTextView.setText(String.format("%.1f", getSpeedFromGravity(progress + 1)));
-    }
-
-    public void setConfigurationSliders() {
-
-    }
-
-    public void loadCurrentSettings() {
-        setGravityAndSpeed(Configurations.alarmGravity);
-
     }
 
     private void showDialog(){
@@ -55,7 +55,6 @@ public class ConfigurationActivity extends Activity {
                 String value = input.getText().toString();
                 spinnerAdapter.add(value);
                 spinnerAdapter.notifyDataSetChanged();
-
             }
         });
 
@@ -66,12 +65,29 @@ public class ConfigurationActivity extends Activity {
 
         alert.show();
     }
+
+    public String[] getPhoneList(){
+        String[] phoneList = new String[spinnerAdapter.getCount()];
+        for (int i = 0; i < spinnerAdapter.getCount(); i++){
+            phoneList[i] = spinnerAdapter.getItem(i);
+        }
+        return phoneList;
+    }
+
+    private void populateSpinnerFromList() {
+        for(int i = 0; i < Configurations.myPhoneNumbers.length; i++) {
+            spinnerAdapter.add(Configurations.myPhoneNumbers[i]);
+            spinnerAdapter.notifyDataSetChanged();
+        }
+    }
+
     private void initPhoneSpinnerListener(){
 
         spinner = (Spinner)findViewById(R.id.phoneListSpinner);
         spinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, android.R.id.text1);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(spinnerAdapter);
+        populateSpinnerFromList(); //from settings file
 
         ((ImageButton) findViewById(R.id.addPhoneNumber)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,12 +104,41 @@ public class ConfigurationActivity extends Activity {
         });
     }
 
-    public ArrayList<String> getPhoneList(){
-        ArrayList<String> phoneList = new ArrayList<String>();
-        for (int i = 0; i < spinnerAdapter.getCount(); i++){
-            phoneList.add(spinnerAdapter.getItem(i));
+    private void initPhoneConfigurations() throws IOException {
+
+        gravitySeekBar = (SeekBar) findViewById(R.id.seekBar);
+        accelerationTextView = (TextView) findViewById(R.id.textView9);
+        speedTextView  = (TextView) findViewById(R.id.textView11);
+        mySmsText = (EditText) findViewById(R.id.editText);
+
+        switches = new Switch[9];
+        switches[0] = (Switch) findViewById(R.id.Switch1);
+        switches[1] = (Switch) findViewById(R.id.Switch2);
+        switches[2] = (Switch) findViewById(R.id.Switch3);
+        switches[3] = (Switch) findViewById(R.id.Switch4);
+        switches[4] = (Switch) findViewById(R.id.Switch5);
+        switches[5] = (Switch) findViewById(R.id.Switch6);
+        switches[6] = (Switch) findViewById(R.id.Switch7);
+        switches[7] = (Switch) findViewById(R.id.Switch8);
+        switches[8] = (Switch) findViewById(R.id.Switch9);
+
+        // Loading saved settings:
+        setGravityAndSpeed(Configurations.alarmGravity - 1);
+        gravitySeekBar.setProgress((int)Configurations.alarmGravity - 1);
+        for (index = 0; index < switches.length; index++) {
+            switches[index].setChecked(Configurations.myConfiguration[index]);
         }
-        return phoneList;
+        mySmsText.setText(Configurations.mySms);
+
+        // Some listeners:
+        gravitySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar gravitySeekBar, int progress, boolean fromUser) {
+                setGravityAndSpeed(progress);
+            }
+
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
     public void onCreate(Bundle savedInstanceState) {
@@ -101,23 +146,55 @@ public class ConfigurationActivity extends Activity {
         Intent intent = getIntent();
         setContentView(R.layout.configuration);
 
+        saveButton = (Button) findViewById(R.id.button);
+        cancelButton = (Button) findViewById(R.id.button1);
+
+        try {
+            Configurations.loadConfigFile();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        System.out.println("********** Dim lista tel: " + Configurations.myPhoneNumbers.length);
         initPhoneSpinnerListener();
 
-        gravitySeekBar = (SeekBar) findViewById(R.id.seekBar);
-        accelerationTextView = (TextView) findViewById(R.id.textView9);
-        speedTextView  = (TextView) findViewById(R.id.textView11);
+        try {
+            initPhoneConfigurations();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
 
-        gravitySeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            public void onProgressChanged(SeekBar gravitySeekBar, int progress, boolean fromUser) {
-                setGravityAndSpeed(progress);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Save my settings:
+                Configurations.alarmGravity = Float.parseFloat(accelerationTextView.getText().toString());  //acceleration
+                for (index = 0; index < switches.length; index++) { //configuration
+                    Configurations.myConfiguration[index] = switches[index].isChecked();
+                }
+
+                Configurations.myPhoneNumbers = new String[spinnerAdapter.getCount()];
+                for (int i = 0; i < spinnerAdapter.getCount(); i++){ //phone numbers
+                    Configurations.myPhoneNumbers[i] = spinnerAdapter.getItem(i);
+                }
+
+                Configurations.mySms = mySmsText.getText().toString(); //sms
+
+                try {
+                    Configurations.saveConfigFile(Configurations.alarmGravity, Configurations.myConfiguration, Configurations.mySms, Configurations.myPhoneNumbers);
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+                finish();
             }
-
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-
-            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
-
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
--- src/com/the_four_amigos/panic_helper/Configurations.java
+++ src/com/the_four_amigos/panic_helper/Configurations.java
@@ -1,5 +1,7 @@
 package com.the_four_amigos.panic_helper;
 
+import java.io.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: cosmin
@@ -10,13 +12,62 @@ package com.the_four_amigos.panic_helper;
 public class Configurations {
     public static float alarmGravity = 1.0f; //vom citi din fisier
     public static boolean [] myConfiguration = {false, false, false, false, false, false, false, false, false};
-    private float gravity = 1.0f;
+    public static String mySms = "Type your message...";
+    public static String [] myPhoneNumbers = new String[0];
 
-    public void setGravity(float alarmGravity) {
-        this.gravity = alarmGravity;
+    public static void loadConfigFile() throws IOException {
+        File file = new File("MyConfigurations\\configFile.txt");
+        BufferedReader configFile = new BufferedReader(new FileReader(file));
+
+        String line = configFile.readLine();
+
+        if (line != null) {
+           alarmGravity = Float.parseFloat(line);
+           line = configFile.readLine();
+
+           if (line != null) {
+               String[] myConfigs = new String[9];
+               myConfigs = line.split(" ");
+               for(int i = 0; i < myConfigs.length; i++) {
+                   myConfiguration[i] = Boolean.parseBoolean(myConfigs[i]);
+               }
+               line = configFile.readLine();
+
+               if (line != null) {
+                   mySms = line;
+                   line = configFile.readLine();
+
+                   if (line != null) {
+                       myPhoneNumbers = new String[line.split(" ").length];
+                       myPhoneNumbers = line.split(" ");
+                   }
+               }
+           }
+        }
+        configFile.close();
     }
 
-    public float getGravity() {
-        return gravity;
+    public static void saveConfigFile(float gravity, boolean [] myConfig, String mySms, String[] myPhoneNos) throws IOException {
+
+        String newLine = System.getProperty("line.separator");
+        String result = "";
+
+        FileWriter f = new FileWriter(new File("MyConfigurations\\configFile.txt"));
+        BufferedWriter configFile = new BufferedWriter(f);
+
+        result += gravity + newLine;
+
+        for (int i = 0; i < myConfig.length; i++) {
+            result += myConfig[i] + " ";
+        }
+
+        result += newLine + mySms + newLine;
+
+        for (int i = 0; i < myConfig.length; i++) {
+            result += myPhoneNos[i] + " ";
+        }
+
+        configFile.write(result);
+        configFile.close();
     }
 }
